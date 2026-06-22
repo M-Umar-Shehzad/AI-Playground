37d7d430a1698ac1b73287f225549b4ad5c9fff2
Muhammad Umar Shehzad <mumarshehzad479@gmail.com>
2026-06-22 16:33:12 +0500

    Remove flux-mobile from VIbeCoding

--- VIbeCoding/flux-mobile/globals.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.tsx" {
-    import React from 'react';
-    const Component: React.ComponentType<any>;
-    export default Component;
-}
--- VIbeCoding/flux-mobile/supabase/migrations/0002_add_original_currency.sql
+++
@@ -1,4 +0,0 @@
--- 0002_add_original_currency.sql
-
-ALTER TABLE transactions ADD COLUMN IF NOT EXISTS original_currency TEXT;
-ALTER TABLE transactions ADD COLUMN IF NOT EXISTS original_amount NUMERIC(12, 2);
